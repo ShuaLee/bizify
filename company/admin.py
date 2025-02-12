92de9f9f160a4eb61a390759a627e9cd9ad12164
Josh Grein <joshgrein2@gmail.com>
2025-02-12 18:08:53 -0500

    Fix Comapny Admin implementation & add DjangoRestFramework

--- company/admin.py
+++ company/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from . import models
 
+
+@admin.register(models.Company)  # This registers the model.
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
 # Register your models here.
-admin.site.register(models.Company)
+# admin.site.register(models.Company)
