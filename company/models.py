32aa346c867db9226794b350fa6f8507de9cb0c3
Josh Grein <joshgrein2@gmail.com>
2025-02-11 20:42:35 -0500

    Initialize company model in admin

--- company/models.py
+++ company/models.py
@@ -6,3 +6,9 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
