90349812e6a8f2b1dfaa60e6bddcdbe1ed2ca838
Josh Grein <joshgrein2@gmail.com>
2025-02-11 19:42:19 -0500

    Change DB to MySQL.

--- bizify/settings.py
+++ bizify/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'bizify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bizify',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Helpome123'
     }
 }
 
