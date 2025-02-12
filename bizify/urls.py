a9b8f2bb8ca65bb27c8ab63fdf66cdee25c67ae0
Josh Grein <joshgrein2@gmail.com>
2025-02-11 20:30:03 -0500

    Adjust admin site title.

--- bizify/urls.py
+++ bizify/urls.py
@@ -18,6 +18,8 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Bizify Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('company/', include('company.urls')),
