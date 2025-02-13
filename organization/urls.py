68dd62dcae259f7f558bfcc196870aa33e160e41
Josh Grein <joshgrein2@gmail.com>
2025-02-12 20:59:10 -0500

    Add test REST implementation with starter Organization app.

--- organization/urls.py
+++ organization/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('company/', views.company_list)
+    path('company/', views.company_list),
+    path('company/<int:id>/', views.company_description)
 ]
