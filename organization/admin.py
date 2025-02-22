f08877965b01fc88afa665c79117d12e7c2bbc14
Josh Grein <joshgrein2@gmail.com>
2025-02-22 18:23:38 -0500

    Profile must be part of a Company before it can get a role, and the role must be in the company itself.

--- organization/admin.py
+++ organization/admin.py
@@ -42,3 +42,21 @@ class ProfileCompanyRoleAdmin(admin.ModelAdmin):
     list_display = ('profile', 'company', 'role')
     list_filter = ('company', 'role')
     search_fields = ('profile__user__email', 'company__name')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'role':
+            # Get the current object's company if editing, or None if creating
+            if request.resolver_match.kwargs.get('object_id'):
+                obj = self.get_object(
+                    request, request.resolver_match.kwargs['object_id'])
+                if obj and obj.company:
+                    kwargs['queryset'] = models.Role.objects.filter(
+                        company=obj.company)
+            # if company is selected int he form (via POST), filter roles by that
+            elif request.POST.get('company'):
+                kwargs['queryset'] = models.Role.objects.filter(
+                    company_id=request.POST['company'])
+            else:
+                # No roles until company is picked
+                kwargs['queryset'] = models.Role.objects.none()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
