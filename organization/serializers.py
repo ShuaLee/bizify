1ca08ffd140cb4a0eccf7fae4636f216f00af2f1
Josh Grein <joshgrein2@gmail.com>
2025-02-22 21:17:06 -0500

    List number of profiles in the company and companies a profile is associated with.

--- organization/serializers.py
+++ organization/serializers.py
@@ -8,15 +8,26 @@ class ProfileSerializer(serializers.ModelSerializer):
         source='user.first_name', read_only=True)
     last_name = serializers.CharField(
         source='user.last_name', read_only=True)
+    companies = serializers.SerializerMethodField()  # Add companies field
 
     class Meta:
         model = Profile
         fields = ['id', 'user_id', 'first_name', 'last_name', 'phone_number',
-                  'country', 'state', 'city', 'postal_code', 'address']
+                  'country', 'state', 'city', 'postal_code', 'companies']
+
+    def get_companies(self, obj):
+        # Fetch companies associated with the profile
+        companies = obj.companies.all()
+        return [{'id': company.id, 'name': company.name} for company in companies]
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    profile_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Company
-        fields = ['id', 'name', 'description', 'admins']
-        read_only_fields = ['admins']  # Prevent manual admin setting via API
+        fields = ['id', 'name', 'description', 'profile_count']
+
+    def get_profile_count(self, obj):
+        # Count profiles linked to this company via ProfileCompanyRole
+        return obj.members.count()
